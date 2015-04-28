50b1d084fa82f64784863458d1b8830bf4708586
Tobias <toblu302@student.liu.se>
2015-04-28 10:19:34 +0200

    The XML-file no longer gets loaded as a resource, because it can't be edited once it's inside a .jar.

--- src/se/liu/ida/toblu302/tddd78/client/SettingsManager.java
+++ src/se/liu/ida/toblu302/tddd78/client/SettingsManager.java
@@ -2,7 +2,6 @@ package se.liu.ida.toblu302.tddd78.client;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -16,9 +15,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 /**
  * Manages (stores/loads) settings.
@@ -39,27 +35,18 @@ public class SettingsManager
 
     private SettingsManager()
     {
-        URL path = SettingsManager.class.getResource(FILENAME);
-        if(path == null)
-        {
-            return;
-        }
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         try
         {
-            InputStream is = (InputStream)path.getContent();
-
-
-
             DocumentBuilder db = dbf.newDocumentBuilder();
-            this.settingsFile = new File(path.toURI());
+            this.settingsFile = new File(FILENAME);
             this.settingsDocument = db.parse(settingsFile);
             this.settingsDocument.getDocumentElement().normalize();
             Element docEle = this.settingsDocument.getDocumentElement();
             NodeList ircList = docEle.getElementsByTagName("IRC");
             this.settingsNode = (Element)ircList.item(0);
         }
-        catch(ParserConfigurationException | SAXException | IOException | URISyntaxException e )
+        catch(ParserConfigurationException | SAXException | IOException e )
         {
             e.printStackTrace();
         }
